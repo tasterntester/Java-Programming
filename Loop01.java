c1c0c5a84505e5f0ef2e742aa04296f43a875a3a
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:37:26 +0530

    WAP to print “Bright IT Career” ten times using for loop

---
+++ Loop01.java
@@ -0,0 +1,16 @@
+package javaAssignment;
+
+public class Loop01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Que.1:Write a program to print “Bright IT Career” ten times using for loop
+		
+				for (int i=0;i<10;i++) { 
+					
+					System.out.println("Bright IT Career"); 
+				} 
+				
+	}
+
+}
