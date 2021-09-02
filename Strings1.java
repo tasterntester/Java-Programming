8a1873f8c0bed792f3008a9355729facc7ccbde4
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 22:49:22 +0530

    Different ways creating a string

---
+++ Strings1.java
@@ -0,0 +1,23 @@
+package javaTraining;
+
+public class Strings1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1. Different ways creating a string
+				/*Using String literal
+			You can create String objects with String literal 
+				String str="Hello!";
+
+				Using new keyword
+				This is the common way to create a String object in java.
+				String str1= new String("Hello!");
+
+				Using character array
+				You could also convert character array into String here
+				char ch[]={ 'H','e','l','l','o','!',};
+				String str1=new String(ch);
+				*/
+	}
+
+}
