c5e359ba074c75ec496453d1054db1eed72f32f3
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:18:45 +0530

    Trimming strings with trim()

---
+++ Strings9.java
@@ -0,0 +1,13 @@
+package javaTraining;
+
+public class Strings9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//9. Trimming strings with trim()
+	      String s11c="  hello string   ";  
+	      System.out.println(s11c+"java");//without trim()  
+	      System.out.println(s11c.trim()+"java");//with trim()
+	}
+
+}
