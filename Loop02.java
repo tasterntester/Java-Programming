53617ae9a69e23c0a5dd021cb8293e3810af5560
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:39:14 +0530

    WAP to print 1 to 20 numbers using the while loop

---
+++ Loop02.java
@@ -0,0 +1,14 @@
+package javaAssignment;
+
+public class Loop02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Que.2:Write a java program to print 1 to 20 numbers using the while loop
+				int num =1;
+				while(num<=20){
+					System.out.println(num++);
+				}
+	}
+
+}
