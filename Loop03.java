210e2bc8ea56b88e651afaf5e924b3a92b38f9b8
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:41:16 +0530

    WAP to equal operator and not equal operators

---
+++ Loop03.java
@@ -0,0 +1,16 @@
+package javaAssignment;
+
+public class Loop03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Que.3:Program to equal operator and not equal operators
+		int x = 5;
+	    int y = 3;
+	    System.out.println(x == y); // returns false because 5 is not equal to 3
+	    
+	    System.out.println(x != y); // returns true because 5 is not equal to 3
+	}
+
+}
