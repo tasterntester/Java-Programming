d19dfd76665d8d980446c4cdaa08ac2116422ac5
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:20:24 +0530

    Replacing characters in strings with replace()

---
+++ Strings10.java
@@ -0,0 +1,13 @@
+package javaTraining;
+
+public class Strings10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//10. Replacing characters in strings with replace()
+	      String s11d="java is a very good language";  
+	      String replaceString=s11d.replace('a','e');//replaces all occurrences of 'a' to 'e'  
+	      System.out.println(replaceString); 
+	}
+
+}
