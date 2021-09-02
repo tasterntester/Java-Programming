07a2567bbcdfaf5078351eabaca06f6ba8fedcd9
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:30:41 +0530

    Converting to uppercase and lowercase

---
+++ Strings14.java
@@ -0,0 +1,13 @@
+package javaTraining;
+
+public class Strings14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//14. Converting to uppercase and lowercase
+	      String S11g = new String("UPPERCASE CONVERTED TO LOWERCASE");
+	        //Convert to LowerCase
+	        System.out.println(S11g.toLowerCase());
+	}
+
+}
