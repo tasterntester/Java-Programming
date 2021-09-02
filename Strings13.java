3d52e45e8d2ac44bc65496b9e3da647e77a5a3d5
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:28:50 +0530

    Converting integer objects to Strings

---
+++ Strings13.java
@@ -0,0 +1,15 @@
+package javaTraining;
+
+public class Strings13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//13. Converting integer objects to Strings
+	      int d = 1234;
+	      Integer obj = new Integer(d);
+	      String str4 = obj.toString();
+	      System.out.println("String str4 = " + str4);
+	}
+
+}
