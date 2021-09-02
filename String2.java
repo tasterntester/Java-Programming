bec7d5506ac4dbc1e91711c92544f9601f790531
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 22:50:49 +0530

    Concatenating two strings using + operator

---
+++ String2.java
@@ -0,0 +1,12 @@
+package javaTraining;
+
+public class String2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//2. Concatenating two strings using + operator
+				String s="Sachin"+" Tendulkar";  
+				   System.out.println(s);//Sachin Tendulkar  
+	}
+
+}
