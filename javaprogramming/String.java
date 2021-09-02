516c4ac9fe65f1ffd997c8264626480b9fe8271a
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:42:31 +0530

    Add files via upload

---
+++ javaprogramming/String.java
@@ -0,0 +1,10 @@
+package javaprogramming;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaprogramming/Sum3No.java
@@ -0,0 +1,13 @@
+package javaprogramming;
+
+public class Sum3No {
+//Write a program to sum three numbers in Java.
+	 public static void main(String[] args) {
+ 
+       int a = 4;
+       int b = 17;
+       int c =6;
+       int sum = a + b + c;
+       System.out.println(sum);
+	 }
+}
