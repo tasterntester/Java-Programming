22c69791589fc62f382a0dd755e5f1c6bbcbb9df
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:26:31 +0530

    Converting Numbers to Strings with valueOf()

---
+++ Strings12.java
@@ -0,0 +1,13 @@
+package javaTraining;
+
+public class Strings12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//12. Converting Numbers to Strings with valueOf()
+	      int value=30;  
+	      String s11f=String.valueOf(value);  
+	      System.out.println(s11f+10);//concatenating string with 10  
+	}
+
+}
