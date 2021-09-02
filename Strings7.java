d717cfb01a46404a5e1725cc0bf207bd0c5cd50c
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 23:12:16 +0530

    Comparing strings using the methods equals()

---
+++ Strings7.java
@@ -0,0 +1,17 @@
+package javaTraining;
+
+public class Strings7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//7. Comparing strings using the methods equals(),
+	      String s1="java1";  
+	      String s2="java1";  
+	      String s3="JAVA2";  
+	      String s4="python";  
+	      System.out.println(s1.equals(s2));//true because content and case is same  
+	      System.out.println(s1.equals(s3));//false because case is not same  
+	      System.out.println(s1.equals(s4));//false because content is not same  
+	}
+
+}
