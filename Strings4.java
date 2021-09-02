bfbe30144d302ef7c630ea3dc1aa5ccf6470449e
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-02 22:57:30 +0530

    Extract a string using Substring

---
+++ Strings4.java
@@ -0,0 +1,14 @@
+package javaTraining;
+
+public class Strings4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//4. Extract a string using Substring
+		   String str1="Tester";
+
+		   String str2=str1.substring(0, 4);
+		   System.out.println(str2);
+	}
+
+}
