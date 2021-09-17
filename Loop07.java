1c8f361e22e5c13942885076a8b15fc8d404f9f9
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-17 14:30:05 +0530

    WAP to print 1 to 10

---
+++ Loop07.java
@@ -0,0 +1,16 @@
+package javaAssignment;
+
+public class Loop07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Que.7:Write a program to print 1 to 10 using the do-while loop statement.
+		 int i=1;    
+		    do{    
+		        System.out.println(i);    
+		    i++;    
+		    }while(i<=10); 
+	}
+
+}
