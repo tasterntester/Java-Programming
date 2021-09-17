c76e806f328caca6a7bcf48f04a06bc3ea168da9
Nitesh Ghorpade <88904226+tasterntester@users.noreply.github.com>
2021-09-17 14:34:43 +0530

    Update Loop09.java

--- Loop09.java
+++ Loop09.java
@@ -5,6 +5,21 @@ public class Loop09 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Que.9:Write a program to find the prime or not.
+	    int num11 = 29;
+	    boolean flag = false;
+	    for (int j = 2; j <= num11 / 2; ++j) {
+	      // condition for nonprime number
+	      if (num11 % j == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+
+	    if (!flag)
+	      System.out.println(num11 + " is a prime number.");
+	    else
+	      System.out.println(num11 + " is not a prime number.");
 	}
 
 }
